919d882e8edcba1f812c996f6d6d8c36aee4c015
Aasmin <aasmin.kainth@gmail.com>
2020-08-21 02:03:28 -0700

    copied code using automation

--- Day_3_JS_Automation/raw/poc/login.js
+++ Day_3_JS_Automation/raw/poc/login.js
@@ -1,17 +1,14 @@
-//Hackerrank links fetcher using Selenium - serially
-
 // npm install selenium-webdriver
 //npm install chromedriver
-require("chromedriver");    //store karn di jroort nahi hai as swd apne aap call kr lega
-let swd = require("selenium-webdriver");    
+require("chromedriver");
+let swd = require("selenium-webdriver");
 // build browser
-let bldr = new swd.Builder();   //Builder naam ki class ka ek object ban rha
+let bldr = new swd.Builder();
 // build a tab
-let driver = bldr.forBrowser("chrome").build(); //driver represents a tab -- i.e jo bhi kaam tab ke andr krna hai wo sab driver pe hoga
+let driver = bldr.forBrowser("chrome").build();
 // pending 
-// let GWillBeOpendP = driver.get("https://www.google.com/");
 let GWillBeOpendP = driver.get("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
-
+let gCodeArr, gInputBox, gCodeBox;
 // GWillBeOpendP.then(function () {
 //     // console.log("Home page opened");
 //     let emailPromise = driver.findElement(swd.By.css("#input-1"));
@@ -23,39 +20,34 @@ let GWillBeOpendP = driver.get("https://www.hackerrank.com/auth/login?h_l=body_m
 //             console.log("Data entered");
 //         })
 //     })
-// });
-// GWillBeOpendP.catch(function(err) {
-//     console.log(err);
-// });
-
+// })
 let { email, pwd } = require("../../../credentials");
 GWillBeOpendP.then(function () {
     let addImpWaitP = driver.manage().setTimeouts({ implicit: 10000 });
     return addImpWaitP;
+}).then(function () {
+    // console.log("Home page opened");
+    let emailPromise = driver.findElement(swd.By.css("#input-1"));
+    let passwordPromise = driver.findElement(swd.By.css("#input-2"));
+    // parallely run promises
+    let bothElemP = Promise.all([emailPromise, passwordPromise]);
+    return bothElemP;
+}).then(function (beArr) {
+    let EWillBeEP = beArr[0].sendKeys(email);
+    let passwordEnteredP = beArr[1].sendKeys(pwd);
+    let bothKeysWillBeEnteredP = Promise.all([EWillBeEP, passwordEnteredP]);
+    return bothKeysWillBeEnteredP;
+}).then(function () {
+    // email and pass entered
+    // create a user defined promise based fn
+    let loginWillClickedP = navigatorfn("button.auth-button");
+    return loginWillClickedP;
+}).then(function () {
+    let ipBtnP = navigatorfn("#base-card-1-link");
+    return ipBtnP;
 })
     .then(function () {
-        // console.log("Home page opened");
-        let emailPromise = driver.findElement(swd.By.css("#input-1"));
-        let passwordPromise = driver.findElement(swd.By.css("#input-2"));
-        let bothElemP = Promise.all([emailPromise, passwordPromise]);
-        return bothElemP;
-    }).then(function (beArr) {
-        let EWillBeEP = beArr[0].sendKeys(email);
-        let passwordEnteredP = beArr[1].sendKeys(pwd);
-        let bothKeysWillBeEnteredP = Promise.all([EWillBeEP, passwordEnteredP]);
-        return bothKeysWillBeEnteredP;
-    }).then(function () {
-        // email and pass entered
-        let loginBtnP = driver.findElement(swd.By.css("button.auth-button"));
-        return loginBtnP;
-    }).then(function (loginBtn) {
-        let clickP = loginBtn.click();
-        return clickP;
-    }).then(function () {
-        let ipBtnP = navigatorfn("#base-card-1-link");   //interview-prep link dhundo click karo
-        return ipBtnP;
-    }).then(function () {
-        let arrayBtnP = navigatorfn("a[data-attr1='arrays']");
+        let arrayBtnP = navigatorfn("a[data-attr1='warmup']");
         return arrayBtnP;
     }).then(function () {
         // console.log("Reached questions page")
@@ -74,13 +66,22 @@ GWillBeOpendP.then(function () {
         let allHrefPArr = Promise.all(hrefpArr);
         return allHrefPArr;
     }).then(function (hrefArr) {
-        console.log(hrefArr);
+        let firstQWillBeSubmitP = questionSubmitter(hrefArr[0]);
+        for (let i = 1; i < hrefArr.length; i++) {
+            firstQWillBeSubmitP = firstQWillBeSubmitP.then(function () {
+                let qsp = questionSubmitter(hrefArr[i]);
+                return qsp;
+            })
+        }
+
+        return firstQWillBeSubmitP;
+    }).then(function () {
+        console.log("All question submitted");
     })
     .catch(function (err) {
         console.log(err)
     })
-
-    // promise=> resolve => find,click
+// promise=> resolve => find,click
 function navigatorfn(selector) {
     let pPromise = new Promise(function (resolve, reject) {
         // logic
@@ -97,4 +98,84 @@ function navigatorfn(selector) {
             })
     });
     return pPromise;
+}
+function questionSubmitter(qlink) {
+    return new Promise(function (resolve, reject) {
+        let qpP = driver.get(qlink);
+        qpP.then(function () {
+            let editorialWillBECLickedP = navigatorfn(
+                "a[data-attr2='Editorial']");
+            return editorialWillBECLickedP;
+        }).then(function () {
+            let handleLockP = handleLockBtn();
+            return handleLockP;
+        }).then(function () {
+            // code find
+            let codep = getCode();
+            return codep;
+            // copy 
+            // code paste
+        }).then(function (code) {
+            console.log(code);
+            // let codePasteP = pasteCode(code);
+            // return codePasteP;
+        })
+            .then(function () {
+                console.log("code copied");
+                resolve();
+            }).catch(function (err) {
+                reject(err);
+            })
+    });
+}
+
+function handleLockBtn() {
+    // exist => click
+    return new Promise(function (resolve, reject) {
+        let lockBtnP = driver.findElement(swd.By.css("button.ui-btn.ui-btn-normal.ui-btn-primary .ui-content.align-icon-right"));
+        lockBtnP.then(function (lockBtn) {
+            let actions = driver.actions({ async: true });
+            let elemPressedP = actions.move({ origin: lockBtn }).click().perform();
+            // Performs release event on target element
+            return elemPressedP
+        }, function () {
+            console.log("Lock Btn not found");
+        }).then(function () {
+            resolve();
+        })
+    })
+    // move on
+}
+function getCode() {
+    return new Promise(function (resolve, reject) {
+        let h3P = driver.findElements(swd.By.css(".hackdown-content h3"));
+        let highlightsP = driver.findElements(swd.By.css(".hackdown-content .highlight"));
+        let bArrP = Promise.all([h3P, highlightsP]);
+        bArrP
+            .then(
+                function (bArr) {
+                    let h3Arr = bArr[0];
+                    let highlightsCodeArr = bArr[1];
+                    gCodeArr = highlightsCodeArr;
+                    let tPArr = [];
+
+                    for (let i = 0; i < h3Arr.length; i++) {
+                        let textP = h3Arr[i].getText();
+                        tPArr.push(textP);
+                    }
+                    let alltEextPArr = Promise.all(tPArr);
+                    return alltEextPArr
+                }).then(
+                    function (allLangArr) {
+                        console.log(allLangArr);
+                        let index = allLangArr.indexOf("C++");
+                        let codePromise = gCodeArr[index].getText();
+                        return codePromise;
+                        // filter out -> C++
+                    }).then(function (code) {
+                        resolve(code);
+                    }).catch(function (err) {
+                        reject(err);
+                    })
+    })
 }
