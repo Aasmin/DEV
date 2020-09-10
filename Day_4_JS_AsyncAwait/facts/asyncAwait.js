4db03f541adc4e73ac9c1d167ec55ba98a580ff2
Aasmin <aasmin.kainth@gmail.com>
2020-09-09 19:11:27 -0700

    hckrnk challenge submit automation complete

--- Day_4_JS_AsyncAwait/facts/asyncAwait.js
+++ Day_4_JS_AsyncAwait/facts/asyncAwait.js
@@ -1,9 +1,30 @@
-let fs = require("fs")
+let fs = require("fs");
+// catch suppresses the error 
+// fsP.then(function (data) {
+//     console.log(data);
+// })
+// syntax sugar
+console.log("Before");
 async function fn() {
-    let ffp = fs.promises.readFile("f1.txt");
+    console.log("Inside fn");
+    let fsP = fs.promises.readFile("f1.txt");
+    let data = await fsP;
+    console.log("After await 1");
+    console.log("" + data);
+    // console.log(data + " ");
+    // console.log(dArr[0] + " ");
+    // console.log(dArr[1] + " ");
+}
+
+async function fn2() {
+    console.log("Inside f2");
     let sfp = fs.promises.readFile("f2.txt");
-    let dArr = await Promise.all([ffp, sfp]);
-    console.log(dArr[0] + " ");
-    console.log(dArr[1] + " ");
+    let data = await sfp;
+    console.log("After await 2");
+    console.log("" + data);
 }
 fn();
+console.log("`````````````````````");
+fn2();
+console.log("`````````````````````");
+console.log("After");
--- Day_4_JS_AsyncAwait/poc/puppeteer.js
+++ Day_4_JS_AsyncAwait/poc/puppeteer.js
@@ -1,6 +1,3 @@
-/**
- * HACKERRANK CHALLENGE CREATION USING PUPPETEER
- */
 // npm install puppeteer
 // puppeteer=> require
 let pp = require("puppeteer");
@@ -21,6 +18,7 @@ async function fn() {
     await tab.type("#input-2", pwd);
     // click=> login
     // await tab.click();
+    // Ye mai kra dunga
     await Promise.all([
         tab.waitForNavigation({ waitUntil: "networkidle0" }),
         tab.click("button.auth-button"),
@@ -31,12 +29,97 @@ async function fn() {
             tab.waitForNavigation({ waitUntil: "networkidle0" }),   //agar button click hone se page change hota hai then use waitForNavigation
             tab.click("a[data-analytics='NavBarProfileDropDownAdministration']")]);
     // Performs release event on target element
-    // driver.findElements - findelement ki jgah use $$
+    // driver.findElements
     let liArr = await tab.$$("ul.nav-tabs li");
     await liArr[1].click();
     let createChallengePageLink = await tab.url();
     console.log(createChallengePageLink);
+
     // parallely tab open 
-    // browser.newPage();
+    // `   // browser.newPage();
+    await handleSinglePage(tab, browser);
+}
+async function handleSinglePage(tab, browser) {
+    // await driver.waitForselector
+    // await tab.waitFor(3000);
+    await tab.waitForSelector(".backbone.block-center", { visible: true })
+    // driver.findelement
+    let qList = await tab.$$(".backbone.block-center");
+    // console.log(qElem)
+    // driver.getAttribute();
+    let linkPromiseArr = [];
+    for (let i = 0; i < qList.length; i++) {
+        let link = tab.evaluate(function (elem) {
+            return elem.getAttribute("href")
+        }, qList[i]);
+        linkPromiseArr.push(link);
+    }
+    let allLinksArr = await Promise.all(linkPromiseArr);
+    let allQonONePArr = [];
+    for (let i = 0; i < allLinksArr.length; i++) {
+        let cLink = `https://www.hackerrank.com${allLinksArr[i]}`;
+        // new Tab
+        let newTab = await browser.newPage();
+        let qWillSolvedP = questionSolver(cLink, newTab);
+        allQonONePArr.push(qWillSolvedP);
+    }
+    await Promise.all(allQonONePArr);
+    // Postorder
+    // decide => move forward
+    //  return
+    let allLis = await tab.$$(".pagination ul li");
+    let nxtBtn = allLis[allLis.length - 2]; //second last bnda mil jaega
+    let isDisabled = await tab.evaluate(function (elem) {
+        return elem.getAttribute("class");
+    }, nxtBtn);
+    if (isDisabled == "disabled") { //agar class lagi hoi disabled name di on the elem
+        return;
+    } else {
+        await Promise.all([nxtBtn.click(), tab.waitForNavigation({ waitUntil: "networkidle0" })])
+        await handleSinglePage(tab, browser);
+    }
 }
 fn();
+function questionSolver(cLink, newTab) {
+    return new Promise(function (resolve, reject) {
+        newTab.goto(cLink, { waitUntil: "networkidle0" }).
+            then(function () {
+                let waitForModerator = newTab.waitForSelector("li[data-tab='moderators']", { visible: true });
+                return waitForModerator;
+            })
+            .then(function () {
+                // navigate to moderator
+                let navigationPromise = Promise.all([newTab.click("li[data-tab='moderators']"), newTab.waitForNavigation({ waitUntil: "networkidle0" })])
+                return navigationPromise
+                // wait for moderator
+                // enter data
+                // press enter
+                // save changes
+                // close the tab
+            }).then(function () {
+                let waitForModeratorP = newTab.waitForSelector("#moderator", { visible: true })
+                return waitForModeratorP;
+            }).then(function () {
+                let keyWillEnteredP = newTab.type("#moderator", "aasmin1kainth");
+                return keyWillEnteredP;
+            }).then(function () {
+                let waitForEnterP = newTab.keyboard.press("Enter");
+                return waitForEnterP;
+            }).then(function () {
+                let waitForClick = newTab.click(".save-challenge.btn.btn-green");
+                return waitForClick;
+            }).then(function () {
+                let waitForTabToCloseP = newTab.close();
+                return waitForTabToCloseP;
+            })
+            .then(function () {
+                console.log("After");
+                resolve();
+            }).catch(function (err) {
+                reject(err);
+            })
+    })
+
+
+
+}
