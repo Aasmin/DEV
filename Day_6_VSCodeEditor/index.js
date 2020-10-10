9d7a0426ad97847427f92e4317f8d5a7a8b84a62
Aasmin <aasmin.kainth@gmail.com>
2020-10-10 15:38:05 -0700

    added jstree level 1 logic

--- Day_6_VSCodeEditor/index.js
+++ Day_6_VSCodeEditor/index.js
@@ -1,15 +1,18 @@
 // npm install --save-dev jstree
 // npm install --save-dev jquery
 const $ = require('jquery');
+const fs = require("fs");
+const { dir } = require("console");
 require('jstree');  //jstree mein har file and folder ko node kehte hein. 
-const nodePath = require('path') //jstree ka module hai path
+const path = require('path') //jstree ka module hai path
 $(document).ready(function(){   //jquery ka function i.e agr document load hojae to function chla do
     //editor
     //terminal
     //tabs
     //tree view
-    let currPath = process.cwd();   //gives current working directory (gives Absolute path i.e from root)
-    let dirName = nodePath.basename(currPath);
+    //pPath - poora path (C://...till file reached)
+    let pPath = process.cwd();   //gives current working directory (gives Absolute path i.e from root)
+    let name = path.basename(pPath);
     // console.log(currPath);
     // console.log(getName(currPath));
     // let data = [];
@@ -25,18 +28,51 @@ $(document).ready(function(){   //jquery ka function i.e agr document load hojae
     //          "data": data
     //       }
     // })
-    var data = [
-        { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
-        { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
-        { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
-        { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
-     ];
-    $("#tree").jstree({
-      "core" : {
-         "data": data
-      },     
-   })
+
+    let data = [{
+        id: pPath,
+        parent: "#",
+        text: name  //folder ka name
+    }]
+    let childArr = createData(pPath);   //we give parent's path to createData(), yeh sare children ka array ban ke de dega
+
+    data = [...data, ...childArr];
+    // MAJOR WORK: we are providing data to this.
+    $("#tree").jstree({ //jstree make use of "tree" div to append the results to display on UI
+        "core": {
+            "data": data
+        },
+    })
+
 })
+function createData(parentPath) {
+    let childrens = fs.readdirSync(parentPath); //parent folder ka content read kiya 
+    let cdata = [];
+    for (let i = 0; i < childrens.length; i++) {
+        let cPath = path.join(parentPath, childrens[i]);   //joining parent's path with the children name
+        let obj = {
+            id: cPath,
+            parent:parentPath,
+            text: childrens[i]
+        };
+        cdata.push(obj);
+    }
+    return cdata;
+} 
+//WORKING of above - har node ke liye - { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" } - bnega
+//  then create data call hoga sabhi ke liye
+//     var data = [
+//         { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
+//         { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
+//         { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
+//         { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
+//      ];
+//     $("#tree").jstree({
+//       "core" : {
+//          "data": data
+//       },     
+//    })
+// })
 
 // function getName(path){
 //     return nodePath.basename(path);
