dacfbe9ac4ca4539cfe62985a0cdd540023cd4a4
Aasmin <aasmin.kainth@gmail.com>
2020-10-10 15:49:43 -0700

    jstree logic of all levels completed

--- Day_6_VSCodeEditor/index.js
+++ Day_6_VSCodeEditor/index.js
@@ -13,22 +13,6 @@ $(document).ready(function(){   //jquery ka function i.e agr document load hojae
     //pPath - poora path (C://...till file reached)
     let pPath = process.cwd();   //gives current working directory (gives Absolute path i.e from root)
     let name = path.basename(pPath);
-    // console.log(currPath);
-    // console.log(getName(currPath));
-    // let data = [];
-    // let baseobj = { //creation of the base node
-    //     id : currPath,
-    //     parent : "#",   //jis cheez ka parent nahi wahan put '#'
-    //     text : getName(currPath)
-    // };
-    // data.push(baseobj);
-
-    // $('#file-explorer').jstree({
-    //     "core" : {  //core property mein data pass krdiya
-    //          "data": data
-    //       }
-    // })
-
     let data = [{
         id: pPath,
         parent: "#",
@@ -39,18 +23,27 @@ $(document).ready(function(){   //jquery ka function i.e agr document load hojae
     data = [...data, ...childArr];
     // MAJOR WORK: we are providing data to this.
     $("#tree").jstree({ //jstree make use of "tree" div to append the results to display on UI
-        "core": {
+        "core": {//core property mein data pass krdiya
+            "check_callback" : true, //callback check krta if function fer te call karda
             "data": data
         },
-    })
+    }).on("select_node.jstree",
+    function (e, data) {
+        let cNodePath = data.node.id; //jb click ho kisi node pe, uski id nikal lo
+        let cArr = createData(cNodePath); //node ke childs ka array bna lo
+        for (let i = 0; i < cArr.length; i++) {
+            console.log(cArr[i]);
+            $('#tree').jstree().create_node(cNodePath, cArr[i], "last");    //array ko jstree mein append krwado
 
+        }
+    })
 })
 function createData(parentPath) {
     let childrens = fs.readdirSync(parentPath); //parent folder ka content read kiya 
     let cdata = [];
     for (let i = 0; i < childrens.length; i++) {
         let cPath = path.join(parentPath, childrens[i]);   //joining parent's path with the children name
-        let obj = {
+        let obj = { //creation of the nodes
             id: cPath,
             parent:parentPath,
             text: childrens[i]
@@ -60,7 +53,7 @@ function createData(parentPath) {
     return cdata;
 } 
 //WORKING of above - har node ke liye - { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" } - bnega
-//  then create data call hoga sabhi ke liye
+//  then create data call hoga sabhi ke liye    //jis cheez ka parent nahi wahan put '#'
 //     var data = [
 //         { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
 //         { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
