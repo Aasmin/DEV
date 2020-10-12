bbb88275d66641c20a5bb4a8a824e00a8e76ada3
Aasmin <aasmin.kainth@gmail.com>
2020-10-11 18:09:04 -0700

    jstree logic complete for grandchild n their child

--- Day_6_VSCodeEditor/index.js
+++ Day_6_VSCodeEditor/index.js
@@ -13,12 +13,12 @@ $(document).ready(function(){   //jquery ka function i.e agr document load hojae
     //pPath - poora path (C://...till file reached)
     let pPath = process.cwd();   //gives current working directory (gives Absolute path i.e from root)
     let name = path.basename(pPath);
-    let data = [{
+    let data = [{   //making the root node
         id: pPath,
         parent: "#",
         text: name  //folder ka name
     }]
-    let childArr = createData(pPath);   //we give parent's path to createData(), yeh sare children ka array ban ke de dega
+    let childArr = addCh(pPath);   //we give parent's path to addCh(), yeh sare children ka array ban ke de dega
 
     data = [...data, ...childArr];
     // MAJOR WORK: we are providing data to this.
@@ -27,18 +27,35 @@ $(document).ready(function(){   //jquery ka function i.e agr document load hojae
             "check_callback" : true, //callback check krta if function fer te call karda
             "data": data
         },
-    }).on("select_node.jstree",
-    function (e, data) {
-        let cNodePath = data.node.id; //jb click ho kisi node pe, uski id nikal lo
-        let cArr = createData(cNodePath); //node ke childs ka array bna lo
-        for (let i = 0; i < cArr.length; i++) {
-            console.log(cArr[i]);
-            $('#tree').jstree().create_node(cNodePath, cArr[i], "last");    //array ko jstree mein append krwado
-
+    }).on("open_node.jstree", function (e, onClickData) {  //changed the select approach coz oh item de content nu khol reha c rather than ohde childs ikalle read krne
+        console.log(onClickData);
+        // let cNodePath = data.node.id; //jb click ho kisi node pe, uski id nikal lo
+        // let cArr = createData(cNodePath); //node ke childs ka array bna lo
+        // for (let i = 0; i < cArr.length; i++) {
+        //     console.log(cArr[i]);
+        //     $('#tree').jstree().create_node(cNodePath, cArr[i], "last");    //array ko jstree mein append krwado
+        let children = onClickData.node.children;
+        for (let i = 0; i < children.length; i++) {
+            let gcArr = addCh(children[i]);
+            for (let j = 0; j < gcArr.length; j++) {
+                let doesExist = $('#tree').jstree(true).get_node(gcArr[j].id);
+                if(doesExist){
+                    return;
+                }
+                // create logic
+                $("#tree").jstree().create_node(children[i], gcArr[j], "last"); //ethe jo child array bneya c oh parent nal append krke show krduga
+                //jis krke arrow lagg jauga
         }
-    })
+    }
 })
-function createData(parentPath) {
+})
+ 
+function addCh(parentPath) {
+    let isDir = fs.lstatSync(parentPath).isDirectory();     //check if its a folder 
+    if (isDir == false) { //if its not a folder return empty
+        return [];
+    }
+    //jo folders hein wo niche jayenge, aur unka content read hoga aur unke children ke object bnenge
     let childrens = fs.readdirSync(parentPath); //parent folder ka content read kiya 
     let cdata = [];
     for (let i = 0; i < childrens.length; i++) {
@@ -52,7 +69,7 @@ function createData(parentPath) {
     }
     return cdata;
 } 
-//WORKING of above - har node ke liye - { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" } - bnega
+//WORKING of above - In JsTree har node ke liye - { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" } - bnega
 //  then create data call hoga sabhi ke liye    //jis cheez ka parent nahi wahan put '#'
 //     var data = [
 //         { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
