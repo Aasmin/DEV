d095af1f32e07d27060a223086efbba3034ad9ef
Aasmin <aasmin.kainth@gmail.com>
2020-11-04 23:52:12 -0800

    Completed image upload and added util logic

--- Day_8_RTWhiteBoard/sticky.js
+++ Day_8_RTWhiteBoard/sticky.js
@@ -6,72 +6,8 @@ function createSticky() {
     //     </div>
     //     <div class="container"><textarea name="" id="" cols="30" rows="10"></textarea></div>
     // </div>  
-    //  create all the required divs
-    let stickyPad = document.createElement("div");
-    let navBar = document.createElement("div");
-    let close = document.createElement("div");
-    let minimize = document.createElement("div");
-    let container = document.createElement("div");
+    let container = createBox();
     let textarea = document.createElement("textarea");
-    // create subtree 
-    stickyPad.appendChild(navBar);
-    stickyPad.appendChild(container);
-    navBar.appendChild(close);
-    navBar.appendChild(minimize);
     container.appendChild(textarea);
-    //  add styling to them using css classes
-    stickyPad.setAttribute("class", "stickyPad");
-    navBar.setAttribute("class", "navBar");
-    close.setAttribute("class", "close");
-    minimize.setAttribute("class", "minimize");
-    container.setAttribute("class", "container");
-    textarea.setAttribute("class", "textarea");
-    // append it body
-    document.body.appendChild(stickyPad);
-    
-    /**
-     * Moving the stickyPad: it involves only changing the top and left of stickpad (check css)
-     */
-    let initialX = null;
-    let initialY = null;
-    let isStickyDown = false;
-    navBar.addEventListener("mousedown", function (e) {
-        // initial point
-        initialX = e.clientX
-        initialY = e.clientY
-        isStickyDown = true;
-    })
-    board.addEventListener("mousemove", function (e) {  //e is an event. iske andr bhut saari values hein
-        if (isStickyDown == true) {
-            // final point 
-            let finalX = e.clientX;
-            let finalY = e.clientY;
-            //  distance
-            let dx = finalX - initialX;
-            let dy = finalY - initialY;
-            //  move sticky
-            let { top, left } = stickyPad.getBoundingClientRect()
-            // stickyPad.style.top=10+"px";
-            stickyPad.style.top = top + dy + "px";
-            stickyPad.style.left = left + dx + "px";
-            initialX = finalX;
-            initialY = finalY;
-        }
-    })
-    window.addEventListener("mouseup", function () {
-        isStickyDown = false;
-    })
-
-    close.addEventListener("click", function () {
-        stickyPad.remove();
-    })
-    let flag = true;
-    minimize.addEventListener("click", function () {
-        if (flag == true) {
-            container.style.display = "none";
-        } else {
-            container.style.display = "block";
-        }
-        flag=!flag;
-    })
+    textarea.setAttribute("class", "textarea"); 
 } 
