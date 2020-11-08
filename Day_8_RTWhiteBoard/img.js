69bb6fcd9bca3048c19887f6b5ea108b84f77e48
Aasmin <aasmin.kainth@gmail.com>
2020-11-07 18:08:05 -0800

    Canvas download

--- Day_8_RTWhiteBoard/img.js
+++ Day_8_RTWhiteBoard/img.js
@@ -11,3 +11,16 @@ upload.addEventListener("change", function (e) {
 //  remove
     uInp.value = null; 
 })
+
+let download = document.querySelector("#download"); 
+download.addEventListener("click", function () {
+    //configuration done
+    let a = document.createElement("a");    //anchor
+    a.href = board.toDataURL('image/png');  // extension defintion
+    a.download = 'file.png';    //prompts user to download the url rather than navigating it
+
+    //now internally clicked to download 
+    a.click();
+    a.remove(); //prevents memory leak 
+}
+)
