4c6ef921a09b92649b5165abc2749ec684d266ff
Aasmin <aasmin.kainth@gmail.com>
2020-11-07 17:34:20 -0800

    label and bubbling on img

--- Day_8_RTWhiteBoard/img.js
+++ Day_8_RTWhiteBoard/img.js
@@ -1,14 +1,13 @@
 let upload = document.querySelector("#upload");
-upload.addEventListener("click", function (e) {
+upload.addEventListener("change", function (e) {
     let uInp = document.querySelector("input[type='file']");
-    uInp.click();
-    // any image is selected
-    uInp.addEventListener("change", function () {
-        let container = createBox();
-        let file = uInp.files[0];
-        let img = document.createElement("img");
-        img.src = URL.createObjectURL(file);
-        img.setAttribute("class", "upload-img")
-        container.appendChild(img);
-    })
+    // let changed = false;
+    let container = createBox();
+    let file = uInp.files[0];
+    let img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    img.setAttribute("class", "upload-img");
+    container.appendChild(img);
+//  remove
+    uInp.value = null; 
 })
