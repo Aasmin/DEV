48116dde73a131b95a5579c1e2d6999a7eb246a5
Aasmin <aasmin.kainth@gmail.com>
2021-01-27 15:49:00 -0800

    added getOne

--- Day_9_FullStackInsta/raw/poc/app.js
+++ Day_9_FullStackInsta/raw/poc/app.js
@@ -2,7 +2,11 @@
 const express = require("express");         //api making framework
 const users = require("./db/user.json");
 const fs = require("fs");
+// npm init -y
+// npm i express uuid
+const { v4: uuidv4 } = require('uuid');     //https://www.npmjs.com/package/uuid; v4 = key
 const app = express();
+
 // get, post, patch, delete => express methods
 // 127.0.0.1:3000 = http://localhost:3000/home
 // get All=> admin
@@ -14,9 +18,10 @@ const app = express();
 app.use(express.json()); //to get the data in the body
 
 // On Postman(Client) to test the method: Post > Body > Raw > JSON
-app.post("/user", function (req, res) { //serverside
+app.post("/user", (req, res) => { //serverside; /user is route
     let user = req.body;
     console.log(user);
+    user.uid = uuidv4();    //adds the unique id
     users.push(user);
     // saved to disk
     fs.writeFileSync("./db/user.json", JSON.stringify(users));
@@ -26,6 +31,28 @@ app.post("/user", function (req, res) { //serverside
         user: req.body
     })
 })
+
+// get => some changing parameter 
+//getOne
+app.get("/user/:uid", (req, res) => {   //":uid" - template route; agar pichhla change ho rea
+    // req paramatere -> user id
+    let cUid = req.params.uid;
+    let userArr = users.filter((user) => {
+        return user.uid == cUid;
+    });
+    console.log(req.params);
+    res.status(201).json({
+        status: "success",
+        user: userArr.length == 0 ? "no user" : userArr[0]
+    })
+})
+
+//getAll
+
+// delete => filter
+// updated => key search 
+// users.splice(idx,1);
+
 app.listen(3000, () => {
     console.log("Server started at port 3000.")
 })
--- Day_9_FullStackInsta/readme.md
+++ Day_9_FullStackInsta/readme.md
@@ -4,3 +4,8 @@
 * Flow
 
 
+#### Notes:
+* uuid - For unique Id
+* NodeMon - Autorestart due to change. Restart maarn di lodd nahi, apne aap restart kr lwega.
+    * 1. "npm i nodemon --save-dev" 
+    * 2. In package.json -> "start": "nodemon app"
