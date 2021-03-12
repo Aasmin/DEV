231dc588a26846c4be974153f0daceb05a832604
Aasmin <aasmin.kainth@gmail.com>
2021-03-12 01:03:52 -0800

    Triggering useState

--- Day_12_React_NetNinja/dojo-blog/src/Home.js
+++ Day_12_React_NetNinja/dojo-blog/src/Home.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const HomePage = () => {
+    const [name, setName] = useState('Mario');
+
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -13,9 +15,16 @@ const HomePage = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log("use effect run");
+        console.log(name);
+    }, [name])
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title = "All Blogs" handleDelete={handleDelete}/>
+            <button onClick={()=>setName('Luigi')}>Click Me</button>
+            <p>{ name }</p>
         </div>
      );
 }
