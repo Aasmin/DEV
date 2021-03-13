1c243bdfde58ed75783b541c444b192d7e5c7e4c
Aasmin <aasmin.kainth@gmail.com>
2021-03-12 16:59:15 -0800

    Error handling

--- Day_12_React_NetNinja/dojo-blog/README.md
+++ Day_12_React_NetNinja/dojo-blog/README.md
@@ -68,3 +68,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+### Manual Commands while building and testing the app
+#### To make the server: 
+npx json-server --watch data/db.json --port 8000
--- Day_12_React_NetNinja/dojo-blog/src/Home.js
+++ Day_12_React_NetNinja/dojo-blog/src/Home.js
@@ -1,32 +1,40 @@
-import { useEffect, useState } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
-const HomePage = () => {
-    const [name, setName] = useState('Mario');
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [])
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-        console.log("use effect run");
-        console.log(name);
-    }, [name])
-
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs} title = "All Blogs" handleDelete={handleDelete}/>
-            <button onClick={()=>setName('Luigi')}>Click Me</button>
-            <p>{ name }</p>
-        </div>
-     );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
-export default HomePage;
+export default Home;
