9d4f0f9f37eb99859904be24abff219e3bebb5be
Aasmin <aasmin.kainth@gmail.com>
2021-03-14 12:33:48 -0700

    error reduction

--- Day_12_React_NetNinja/dojo-blog/src/App.js
+++ Day_12_React_NetNinja/dojo-blog/src/App.js
@@ -1,30 +1,30 @@
-import NavBar from './Navbar';
-import HomePage from './Home';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import BlogDetails from './BlogDetails';
-import NotFound from './notFpund';
+import NotFound from './notFound';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
+        <Navbar />
         <div className="content">
           <Switch>
             <Route exact path="/">
-              <HomePage />
+              <Home />
             </Route>
             <Route path="/create">
               <Create />
             </Route>
-          </Switch>
             <Route path="/blogs/:id">
               <BlogDetails />
             </Route>
             <Route path="*">
               <NotFound />
             </Route>
+          </Switch>
         </div>
       </div>
     </Router>
